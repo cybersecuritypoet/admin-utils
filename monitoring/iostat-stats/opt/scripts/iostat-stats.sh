1926904c955bff254d6573f43c95ea79b836cbe5
cybersecuritypoet <69465746+cybersecuritypoet@users.noreply.github.com>
2022-08-31 15:35:15 +0200

    Fix line buffering and parameters

--- monitoring/iostat-stats/opt/scripts/iostat-stats.sh
+++ monitoring/iostat-stats/opt/scripts/iostat-stats.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 IOSTAT="/usr/bin/iostat"
+STDBUF="/usr/bin/stdbuf"
 INTERVAL=5
 FACILITY="local5"
 SEVERITY="info"
 TAG="iostat-stats"
 SOCKET="/var/run/rsyslogd/iostat-stats.sock"
-exec 3< <(iostat -dxy $INTERVAL | awk '
+MATCH="^\{\"device\"\:\"sd[a,b].*"
+exec 3< <($STDBUF -oL $IOSTAT -dxy $INTERVAL | $STDBUF -oL awk '
 /^Device/ {
-    gsub(":", "")
-    gsub("/", "_") 
-    gsub("-", "_")
-    gsub("%", "")
-    split(tolower($0), FIELDS)
+	gsub(":", "")
+	gsub("/", "_") 
+	gsub("-", "_")
+	gsub("%", "")
+	split(tolower($0), FIELDS)
 }
 /^[a-z0-9]+/ {
-    printf "{"
-    for (II=1; II<=NF; ++II) {
-        printf "\"" FIELDS[II] "\":\"" $II "\""
+	printf "{"
+	for (II=1; II<=NF; ++II) {
+		printf "\"" FIELDS[II] "\":\"" $II "\""
 		if (II != NF) {printf ","}
-    }
-    printf "}\n"
+	}
+	printf "}\n"
 }')
 while read line; do
+if [[ $line =~ $MATCH ]];
+then
 	logger -e -p "$FACILITY.$SEVERITY" -t "$TAG" --socket "$SOCKET" "$line"
+	echo "$line"
+fi
 done <&3
