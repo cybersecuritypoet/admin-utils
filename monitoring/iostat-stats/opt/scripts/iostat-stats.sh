70f1ea0991d23a9fb4bf361c255478a87609996f
cybersecuritypoet <69465746+cybersecuritypoet@users.noreply.github.com>
2023-02-21 18:46:49 +0100

    Update iostat-stats script to include CPU info as well.

--- monitoring/iostat-stats/opt/scripts/iostat-stats.sh
+++ monitoring/iostat-stats/opt/scripts/iostat-stats.sh
@@ -1,31 +1,56 @@
 #!/bin/bash
+
 IOSTAT="/usr/bin/iostat"
 STDBUF="/usr/bin/stdbuf"
 INTERVAL=5
 FACILITY="local5"
 SEVERITY="info"
 TAG="iostat-stats"
+TAG_DEV="iostat-stats-dev"
+TAG_CPU="iostat-stats-cpu"
 SOCKET="/var/run/rsyslogd/iostat-stats.sock"
-MATCH="^\{\"device\"\:\"sd[a,b].*"
-exec 3< <($STDBUF -oL $IOSTAT -dxy $INTERVAL | $STDBUF -oL awk '
-/^Device/ {
-	gsub(":", "")
-	gsub("/", "_") 
-	gsub("-", "_")
-	gsub("%", "")
-	split(tolower($0), FIELDS)
+MATCH_DEV="^\{\"device\"\:\"sd[a-z].*"
+MATCH_CPU="^\{\"avg\_cpu\"\:.*"
+
+exec 3< <($STDBUF -oL $IOSTAT -xy $INTERVAL | $STDBUF -oL awk '
+/^avg\-cpu:\s+%/ {
+        gsub("%", "")
+        gsub("/", "_")
+        gsub("-", "_")
+        gsub("%", "")
+        gsub(":", "")
+        split(tolower($0), FIELDS_CPU)
 }
-/^[a-z0-9]+/ {
-	printf "{"
-	for (II=1; II<=NF; ++II) {
-		printf "\"" FIELDS[II] "\":\"" $II "\""
-		if (II != NF) {printf ","}
-	}
-	printf "}\n"
+/^\s+[0-9]+\.[0-9]+/ {
+        printf "{"
+        for (II=1; II<=NF; ++II) {
+                printf "\"" FIELDS_CPU[II] "\":\"" $II "\""
+                if (II != NF) {printf ","}
+        }
+        printf "}\n"
+}
+/^Device/ {
+        gsub(":", "")
+        gsub("/", "_")
+        gsub("-", "_")
+        gsub("%", "")
+        split(tolower($0), FIELDS)
+}
+/^[a-z0-9\-_]+\s+/ {
+        printf "{"
+        for (II=1; II<=NF; ++II) {
+                printf "\"" FIELDS[II] "\":\"" $II "\""
+                if (II != NF) {printf ","}
+        }
+        printf "}\n"
 }')
 while read line; do
-if [[ $line =~ $MATCH ]];
+if [[ $line =~ $MATCH_DEV ]];
 then
-	logger -e -p "$FACILITY.$SEVERITY" -t "$TAG" --socket "$SOCKET" "$line"
+        logger -e -p "$FACILITY.$SEVERITY" -t "${TAG_DEV}" --socket "$SOCKET" "$line"
+fi
+if [[ $line =~ $MATCH_CPU ]];
+then
+        logger -e -p "$FACILITY.$SEVERITY" -t "${TAG_CPU}" --socket "$SOCKET" "$line"
 fi
 done <&3
